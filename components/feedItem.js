9e1744224677054e75a3385e6992f6985b6ab98d
7bestapp <timo.trumpp@gmail.com>
2018-07-25 14:54:40 +0200

    Finetune feed cards

---
+++ components/feedItem.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+} from 'react-native';
+
+export default FeedItem = ({ feed, index }) => {
+
+  return (
+    <View style={styles.feedCard}>
+      <View style={styles.cardHeader}>
+        <View style={styles.cardUser}>
+          <Text style={styles.user}>{feed.user}</Text>
+        </View>
+        <View style={styles.cardDate}>
+          <Text style={styles.date}>{feed.date}</Text>
+        </View>
+      </View>
+      <View style={styles.cardBody}>
+        <View style={styles.cardWorkout}>
+          <Text style={styles.workout}>{feed.workout}</Text>
+        </View>
+        <View style={styles.cardTime}>
+          <Text style={styles.time}>{feed.time}</Text>
+        </View>
+      </View>
+      <View style={styles.cardSocial}>
+        <Text style={styles.social}>:muscle: {feed.muscles}</Text>
+        <Text style={styles.social}>:clap: {feed.claps}</Text>
+        <Text style={styles.social}>:fire: {feed.fires}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  feedCard: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    borderColor: 'grey',
+    borderWidth: 1,
+    margin: 15,
+    marginBottom: 0,
+    borderBottomLeftRadius: 4,
+    borderBottomRightRadius: 4,
+    borderTopLeftRadius: 4,
+    borderTopRightRadius: 4,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgba(0,0,0, .2)',
+        shadowOffset: { height: 0, width: 0 },
+        shadowOpacity: 1,
+        shadowRadius: 1,
+      },
+      android: {
+        elevation: 1,
+      },
+    }),
+  },
+  cardHeader: {
+    padding: 15,
+    flexDirection: 'row',
+    borderBottomColor: '#F4F4F4',
+    borderBottomWidth: 1,
+  },
+  cardUser: {
+    flex: 3,
+  },
+  user: {
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  cardDate: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+  date: {
+    fontFamily: 'Roboto',
+    fontSize: 16,
+  },
+  cardBody: {
+    padding: 15,
+    flexDirection: 'row'
+  },
+  cardWorkout: {
+    flex: 3,
+  },
+  workout: {
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  cardTime: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+  time: {
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  cardSocial: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 15,
+  },
+  social: {
+    flex: 1,
+    fontFamily: 'Roboto',
+    fontSize: 14,
+  }
+
+})
--- screens/profile.js
+++ screens/profile.js
@@ -7,7 +7,7 @@ import {
   StyleSheet
 } from 'react-native';
 
-import NewsItem from '../components/newsItem.js';
+import FeedItem from '../components/feedItem.js';
 
 export default class Profile extends React.Component {
 
@@ -15,18 +15,42 @@ export default class Profile extends React.Component {
     super(props);
     this.state = {
       isLoading: true,
-      news_items: [
+      feed_items: [
         {
-          title: 'Art Makes You Smart',
-          summary: 'Museum visits increase test scores, generate social responsibility and increase appreciation of the arts by students.',
+          user: 'Timo Trumpp',
+          date: 'Gestern',
+          workout: 'FOX | ORIGINAL',
+          time: '23:22',
+          muscles: '0',
+          claps: '1',
+          fires: '0',
         },
         {
-          title: 'Tension and Flaws Before Health Website Crash',
-          summary: 'Interviews and documents offer new details into how the rollout of President Obama\'s signature program turned into a major humiliation.',
+          user: 'Timo Trumpp',
+          date: '3 Tage',
+          workout: 'HYENA | ORIGINAL',
+          time: '19:51',
+          muscles: '1',
+          claps: '0',
+          fires: '0',
         },
         {
-          title: '36 Hours in Charleston, S.C.',
-          summary: 'Crowds are thinner and temperatures are mild during winter in this..',
+          user: 'Timo Trumpp',
+          date: '10 Tage',
+          workout: 'WOLF | ORIGINAL',
+          time: '24:58',
+          muscles: '0',
+          claps: '0',
+          fires: '1',
+        },
+        {
+          user: 'Timo Trumpp',
+          date: '15 Tage',
+          workout: 'FOX | ORIGINAL',
+          time: '23:22',
+          muscles: '0',
+          claps: '1',
+          fires: '0',
         },
       ]
     }
@@ -50,9 +74,9 @@ export default class Profile extends React.Component {
       });
   }
 
-  renderNews() {
-    return this.state.news_items.map((news, index) => {
-      return <NewsItem key={index} index={index} news={news} />
+  renderFeed() {
+    return this.state.feed_items.map((feed, index) => {
+      return <FeedItem key={index} index={index} feed={feed} />
     });
   }
 
@@ -67,7 +91,7 @@ export default class Profile extends React.Component {
     }
 
     return(
-      <View style={{flex: 1, flexDirection: 'column'}}>
+      <ScrollView style={{flex: 1, flexDirection: 'column', backgroundColor: '#E5E5E5'}}>
         <View style={styles.profileHeader}>
           <Text style={[styles.headerText, styles.boldText]}>Timo Trumpp</Text>
           <Text style={styles.baseText}>TSV RSK Esslingen, C-Jugend</Text>
@@ -117,10 +141,10 @@ export default class Profile extends React.Component {
             <Text style={[styles.subText, styles.textCenter]}>TRIKOT NR</Text>
           </View>
         </View>
-        <ScrollView style={styles.news_container}>
-          { this.renderNews() }
-        </ScrollView>
-      </View>
+        <View>
+          { this.renderFeed() }
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -157,20 +181,16 @@ const styles = StyleSheet.create({
   },
   bioHeader: {
     height: 60,
+    backgroundColor: 'white',
     borderBottomColor: '#F4F4F4',
     borderBottomWidth: 1,
     justifyContent: "center",
-    alignItems: "center",
   },
   bioBox: {
     flex: 1,
     height: 65,
+    backgroundColor: 'white',
     justifyContent: "center",
     alignItems: "center",
-  },
-  news_container: {
-    flex: 1,
-    margin: 10,
-    flexDirection: 'column',
   },
 });
